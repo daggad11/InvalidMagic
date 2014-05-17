90e77b32a105c34d511dc0b498c519f4c3072c39
thundRRR <thundr.assasin@gmail.com>
2014-05-17 16:07:09 -0700

    object values were fleshed out a bit

--- include/object.hpp
+++ include/object.hpp
@@ -1,4 +1,4 @@
-//header file for the object object
+//header file for the object object object object
 #ifndef OBJECT_H
 #define OBJECT_H
 #include <SFML/Graphics.hpp>
@@ -8,9 +8,17 @@ class Object : public Entity
 {
 public:
 	Object();
-	Object(sf::RenderWindow* window, int width, int height, int x, int y, sf::Texture* texture, int mass);
+	Object(sf::RenderWindow* window, int width, int height, int x, int y, sf::Texture* texture, std::string type);
+	bool getSolid();
+	void setSolid(bool s);
+	bool getMovable();
+	void setMovable(bool m);
+	//void breakThisObject(int reducedBy);//this method takes in the players damage as an arg and decreases durability until it hits 0.
 	~Object();
 private:
-	bool moveable;
+	bool movable;
+	bool solid;
+	std::string type;
+	int durability;//for if you want to smash it?
 };
 #endif
--- src/object.cpp
+++ src/object.cpp
@@ -1,8 +1,35 @@
 #include "object.hpp"
 
 
-Object::Object(sf::RenderWindow* window, int width, int height, int x, int y, sf::Texture* texture, int mass) : Entity(window, width, height, x, y, texture, mass){
-	moveable = true;
+Object::Object(sf::RenderWindow* window, int width, int height, int x, int y, sf::Texture* texture, std::string type) : Entity(window, width, height, x, y, texture, mass){
+	int mass;
+	if(type=="tree"){
+		solid = true;
+		durability = 50;
+		mass = 200;
+	}
+	else if(type=="rock"){
+		solid = true;
+		durability = 9001;
+		mass = 500;
+	}
+	
+}
+
+bool Object::getSolid(){
+	return solid;
+}
+
+void Object::setSolid(bool s){
+	this->solid = s;
+}
+
+bool Object::getMovable(){
+	return movable;
+}
+
+void Object::setMovable(bool m){
+	this->movable = m;
 }
 
 Object::~Object(){
