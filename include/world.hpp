8baee8719ba487b7ca62bc9c429bc109975d629a
David <daggad95@gmail.com>
2014-05-16 19:14:51 -0700

    world

--- include/world.hpp
+++ include/world.hpp
@@ -1,12 +1,16 @@
 //this is the world header file
 #ifndef WORLD_H
 #define WORLD_H
+#include <vector>
+#include "tile.hpp"
 
 class World
 {
 public:
 	 World();
 	~ World();
+private:
+	std::vector<std::vector<Tile>> tiles;
 	
 };
 #endif
