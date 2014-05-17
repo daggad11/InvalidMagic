1093e0c57e93b0e9f2acad9a373a00ce77ea22bd
Kieran <renoh47@gmail.com>
2014-05-16 21:13:28 -0700

    Added test tile, implemented getPlayer

--- src/world.cpp
+++ src/world.cpp
@@ -7,8 +7,22 @@ World::World()
 
 World::World(sf::RenderWindow* window){
 	this->window = window;
+	textures.push_back(sf::texture());
+	textures[0].loadFromFile("../resources/dirt1.png")
+	tiles.push_back(Tile(window, 1, 1, 16, 16, &(textures[0])));
 }
 
 World::~World(){
 	//tonodo
+}
+
+void World::draw()
+{
+	for (auto tile: tiles)
+		tile.draw();
+}
+
+Player* World::getPlayer()
+{
+	return &p;
 }
