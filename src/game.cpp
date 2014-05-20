30c7cfa8335e36c0fa9be1257e9b832447b9ddfe
Kieran <renoh47@gmail.com>
2014-05-20 07:08:53 -0700

    Added FPS counter (may not be working

--- src/game.cpp
+++ src/game.cpp
@@ -18,6 +18,13 @@ int main(int argc, char* argv[]) {
 	Tile::tileSize = window.getSize().x/20;
 	sf::Clock timer;
 	sf::View view(sf::FloatRect(0, 0, window.getSize().x, window.getSize().y));
+	sf::Font font;
+	font.loadFromFile("../resources/arial.ttf");
+	sf::Text text;
+	text.setFont(font);
+	std::stringstream s;
+
+	timer.restart();
 
 	Resources::init();
 
@@ -28,49 +35,50 @@ int main(int argc, char* argv[]) {
 	view.setCenter(world.getPlayer()->getX()*Tile::tileSize, world.getPlayer()->getY()*Tile::tileSize);
 
 	window.setView(view);
-	timer.restart();
-
+	
 
   while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event))
-		{	
-			if (event.type == sf::Event::Closed) {
-				world.save();
-				window.close();	
-			}
-		}
-		//keyboard input
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+	sf::Event event;
+	while (window.pollEvent(event))
+	{	
+		if (event.type == sf::Event::Closed) {
 			world.save();
-			window.close();
+			window.close();	
 		}
-		//player movement
-		bool up, down, left, right;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-			up = true;
-		else
-			up = false;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-			down = true;
-		else
-			down = false;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-			right = true;
-		else
-			right = false;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-			left = true;
-		else
-			left = false;
-		world.getPlayer()->setMovementState(up, down, right, left);
-		//end of keyboard input
+	}
+	//keyboard input
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+		world.save();
+		window.close();
+	}
+	//player movement
+	bool up, down, left, right;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+		up = true;
+	else
+		up = false;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+		down = true;
+	else
+		down = false;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		right = true;
+	else
+		right = false;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		left = true;
+	else
+		left = false;
+	world.getPlayer()->setMovementState(up, down, right, left);
+	//end of keyboard input
 
-		
-		world.update();
-		timer.restart();
+	s.str("");
+	world.update();
+	s << "FPS:" << (1.0/(timer.restart()).asSeconds());
+    text.setString(s.str());
     window.clear(sf::Color::Black);
 	world.draw();	
+	window.draw(text);
     window.display();
   }
   return 0;
