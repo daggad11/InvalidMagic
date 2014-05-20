e886446278be45024ed9917be647924ae460fcf3
Kieran <renoh47@gmail.com>
2014-05-20 16:13:00 -0700

    Fixed FPS Monitor (kind of)

--- src/game.cpp
+++ src/game.cpp
@@ -8,27 +8,47 @@
 int Tile::tileSize;
 
 int main(int argc, char* argv[]) {
+	//default window sizing
 	int width = 800;
 	int height = 600;
+
+	//////////////////////////////
+	// Cmd Line Arg  Processing //
+	//////////////////////////////
 	if (argc == 3) {
 		width = atoi(argv[1]);
 		height = atoi(argv[2]);
 	}
-  sf::RenderWindow window(sf::VideoMode(width, height), "Invalid Magic");
+	else if (argc > 3 || argc == 2)
+	{
+		std::cout <<" Usage: <height> <width>." << std::endl;
+		return 0;
+	}
+
+	/////////////////////
+	// Window Creation //
+	/////////////////////
+	sf::RenderWindow window(sf::VideoMode(width, height), "Invalid Magic");
 	Tile::tileSize = window.getSize().x/20;
-	sf::Clock timer;
 	sf::View view(sf::FloatRect(0, 0, window.getSize().x, window.getSize().y));
+	
+	/////////////////
+	// FPS Monitor //
+	/////////////////
+	sf::Clock timer;
 	sf::Font font;
 	font.loadFromFile("resources/arial.ttf");
 	sf::Text text;
 	text.setFont(font);
 	text.setColor(sf::Color::White);
+	text.setCharacterSize(12);
 	std::stringstream s;
-
 	timer.restart();
 
+	////////////////
+	// Load World //
+	////////////////
 	Resources::init();
-
 	std::ifstream saveFile("data/world.save");
 	World world(&window, &view, saveFile, &timer);
 	saveFile.close();
@@ -38,49 +58,68 @@ int main(int argc, char* argv[]) {
 	window.setView(view);
 	
 
-  while (window.isOpen()) {
-	sf::Event event;
-	while (window.pollEvent(event))
-	{	
-		if (event.type == sf::Event::Closed) {
-			world.save();
-			window.close();	
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event))
+		{	
+			if (event.type == sf::Event::Closed) {
+				world.save();
+				window.close();	
+			}
 		}
-	}
-	//keyboard input
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
-		world.save();
-		window.close();
-	}
-	//player movement
-	bool up, down, left, right;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
-		up = true;
-	else
-		up = false;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-		down = true;
-	else
-		down = false;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
-		right = true;
-	else
-		right = false;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-		left = true;
-	else
-		left = false;
-	world.getPlayer()->setMovementState(up, down, right, left);
-	//end of keyboard input
+		//keyboard input
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+			world.save();
+			window.close();
+		}
+		/////////////////////
+		// Player Movement //
+		/////////////////////
+		bool up, down, left, right;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+			up = true;
+		else
+			up = false;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+			down = true;
+		else
+			down = false;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+			right = true;
+		else
+			right = false;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+			left = true;
+		else
+			left = false;
+		world.getPlayer()->setMovementState(up, down, right, left);
+		//end of keyboard input
 
-	s.str("");
-	world.update();
-	s << "FPS:" << (1.0/(timer.restart()).asSeconds());
-    text.setString(s.str());
-    window.clear(sf::Color::Black);
-	world.draw();	
-	window.draw(text);
-    window.display();
-  }
+		//////////////////
+		// Update World //
+		//////////////////
+		world.update();
+
+		///////////////////
+		// Determine FPS //
+		///////////////////
+		s.str("");
+		s << "FPS:" << (int)(1.0/(timer.restart()).asSeconds());
+	    text.setString(s.str());
+	    text.setPosition(world.getPlayer()->getX()*Tile::tileSize, world.getPlayer()->getY()*Tile::tileSize);
+
+	    window.clear(sf::Color::Black);
+		
+	    ////////////////
+	    // Draw World //
+	    ////////////////
+		world.draw();	
+
+		//////////////////////
+		// Draw FPS + Debug //
+		//////////////////////
+		window.draw(text);
+	    window.display();
+  	}
   return 0;
 }
